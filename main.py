c7f34f93711b65b026b5b7863840b556a4fce42f
james-foster <james.foster@example.com>
2023-11-22 14:50:32 +0400

    voice message done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import VoiceResponse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,11 +9,24 @@ load_dotenv()
 app = Flask(__name__)
 
 
+@app.route("/voice", methods=["POST"])
+def voice():
+    resp = VoiceResponse()
+    audio = "Hello. Thanks for your call. I'm ready to take your order."
+    resp.say(audio, voice="male")
+    resp.record()
+    resp.hangup()
+
+    return str(resp)
+
+
 @app.route("/sms", methods=["POST"])
 def sms():
     resp = MessagingResponse()
-    message_body = request.form['Body']
-    resp.message(f"Welcome to my app. You just sent me a message '{message_body}'.")
+    message_body = request.form["Body"]
+    resp.message(
+        f"Welcome to my app. You just sent me a message '{message_body}'."
+    )
     return str(resp)
 
 
